0b2f96c036be6e1dde012d81697259c43d36bce3
Ometere <ookuoluse@yahoo.com>
2023-05-13 03:13:50 +0100

    files deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import EducationalJourney from "./componenets/educationalJourney/EducationalJourney";
-import Footer from "./componenets/footer/Footer";
-import FourthHomeComponent from "./componenets/forthHomeComponent/FourthHomeComponent";
-import Home from "./componenets/home/Home";
-import Navbar from "./componenets/navbar/Navbar";
-import SecondHomeSection from "./componenets/secondHomeSection/SecondHomeSection";
-import Testimonials from "./componenets/testimonials/Testimonials";
-import ThirdHomeSection from "./componenets/thirdHomeSection/ThirdHomeSection";
-//import './app.css';
+import "./app.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
-      <SecondHomeSection/>
-      <ThirdHomeSection/>
-      <FourthHomeComponent/>
-      <EducationalJourney/>
-      <Testimonials/>
-      <Footer/>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="text-3xl font-bold underline bg-red-400 w-1/2"> Navbar</div>
+  );
+}
